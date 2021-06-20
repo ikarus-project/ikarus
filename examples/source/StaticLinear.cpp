15d404e8f5141ceb19d6c2f458f69477e9164249
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2021-06-20 11:42:05 +0200

    clean-up workflows

---
+++ examples/source/StaticLinear.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <matplot/matplot.h>
+
+int main() {
+    using namespace matplot;
+    auto [X, Y] = meshgrid(iota(-5, 0.5, 5));
+    auto Z = transform(
+            X, Y, [](double x, double y) { return y * sin(x) - x * cos(y); });
+    auto C = transform(X, Y, [](double x, double y) { return x * y; });
+    surf(X, Y, Z)->face_alpha(0.5).edge_color("red");
+
+    show();
+    return 0;
+}
