dea169cbef725b0ac25e0a4a388f6b3dbb129488
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2021-06-23 15:31:11 +0200

    fix sanitizers

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -2,9 +2,6 @@
 # enabled during configuration by passing an additional `-DUSE_<TOOL>=<VALUE>` argument to CMake
 
 # only activate tools for top level project
-if(NOT PROJECT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
-  return()
-endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
