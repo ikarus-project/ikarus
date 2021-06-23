c4dc499beac77475ec0e916c1a6367a02abeb699
AlexanderMüller <mueller@uni-stuttgart.de>
2021-06-23 20:48:58 +0200

    test

--- tests/src/FEFactoryTest.cpp
+++ tests/src/FEFactoryTest.cpp
@@ -1,16 +1,16 @@
 //
 // Created by Alex on 21.04.2021.
 //
-#define EIGEN_MATRIXBASE_PLUGIN "IBB_Eigen_MatrixBaseAddon.h"
-
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
-
-#include "testHelpers.h"
+//#define EIGEN_MATRIXBASE_PLUGIN "IBB_Eigen_MatrixBaseAddon.h"
+//
+//#include <gmock/gmock.h>
+//#include <gtest/gtest.h>
+//
+//#include "testHelpers.h"
 
 //#include <ikarus/FiniteElements/Factory/FiniteElementFactory.h>
 
-TEST(FEFactory, FEFactoryExample){
-    //  using namespace Ikarus::FiniteElements;
-
-};
+//TEST(FEFactory, FEFactoryExample){
+//    //  using namespace Ikarus::FiniteElements;
+//std
+//}
