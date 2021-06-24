f9672a7a3526ef09dadeaa991e8b8abc739c81e1
AlexanderMüller <mueller@uni-stuttgart.de>
2021-06-24 13:26:14 +0200

    add drawer and draw grid example

---
+++ examples/src/drawgrid.cpp
@@ -0,0 +1,40 @@
+
+#include <dune/geometry/type.hh>
+
+#include <ikarus/Grids/SimpleGrid/SimpleGrid.h>
+#include <ikarus/Grids/GridHelper/griddrawer.h>
+
+
+int main(){
+using namespace Ikarus::Grid;
+using Grid = SimpleGrid<3, 3>;
+SimpleGridFactory<Grid> gridFactory;
+using vertexType = Ikarus::FixedVector3d;
+std::vector<vertexType> verticesVec;
+verticesVec.emplace_back(vertexType{0.0, 0.0, -3.0});  // 0
+verticesVec.emplace_back(vertexType{2.0, 0.0, -3.0});  // 1
+verticesVec.emplace_back(vertexType{0.0, 2.0, -3.0});  // 2
+verticesVec.emplace_back(vertexType{2.0, 2.0, -3.0});  // 3
+verticesVec.emplace_back(vertexType{0.0, 0.0, 3.0});   // 4
+verticesVec.emplace_back(vertexType{2.0, 0.0, 3.0});   // 5
+verticesVec.emplace_back(vertexType{0.0, 2.0, 3.0});   // 6
+verticesVec.emplace_back(vertexType{2.0, 2.0, 3.0});   // 7
+verticesVec.emplace_back(vertexType{4.0, 0.0, 3.0});   // 8
+
+for (auto &&vert : verticesVec) gridFactory.insertVertex(vert);
+
+Ikarus::DynArrayXi elementIndices;
+elementIndices.resize(8);
+elementIndices << 0, 1, 2, 3, 4, 5, 6, 7;
+gridFactory.insertElement(Dune::GeometryTypes::hexahedron, elementIndices);
+elementIndices.resize(4);
+elementIndices << 1, 8, 3, 5;
+gridFactory.insertElement(Dune::GeometryTypes::tetrahedron, elementIndices);
+
+Grid actualGrid = gridFactory.createGrid();
+
+auto gridView = actualGrid.leafGridView();
+
+draw(gridView);
+
+}
