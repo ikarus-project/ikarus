0fdaab768ea56aec53f2ba67b9b5b77fd2fa3e84
rath3t <mueller@ibb.uni-stuttgart.de>
2021-07-02 20:53:18 +0200

    Update DofHandlerTest.cpp

--- tests/src/DofHandlerTest.cpp
+++ tests/src/DofHandlerTest.cpp
@@ -10,7 +10,7 @@
 #include <fstream>
 #include <vector>
 
-#include <Eigen/Core> // IWYU pragma: export
+#include <Eigen/Core>
 
 #include <ikarus/DofManager/DefaultDofManager.h>
 #include <ikarus/FiniteElements/ElasticityFE.h>
