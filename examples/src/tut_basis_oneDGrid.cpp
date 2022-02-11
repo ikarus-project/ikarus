f623c5ee82fa46020b86e481b8fd7f800e343c93
Tobias Willmann <willmann@ibb.uni-stuttgart.de>
2022-02-11 15:45:13 +0100

    Stiffness Truss Element implemented

--- examples/src/tut_basis_oneDGrid.cpp
+++ examples/src/tut_basis_oneDGrid.cpp
@@ -4,9 +4,10 @@
 #include <../../config.h>
 #include <numbers>
 
-
-#include <dune/geometry/quadraturerules.hh>
+#include <dune/functions/functionspacebases/basistags.hh>
 #include <dune/functions/functionspacebases/lagrangebasis.hh>
+#include <dune/functions/functionspacebases/powerbasis.hh>
+#include <dune/geometry/quadraturerules.hh>
 #include <dune/grid/onedgrid.hh>
 
 #include <Eigen/Core>
@@ -15,20 +16,20 @@
 #include "ikarus/LocalBasis/localBasis.h"
 #include <ikarus/Grids/GridHelper/griddrawer.h>
 
-
-
-
-int main(){
+void exampleTrussElement(){
   constexpr int griddim = 1;
   const double L = 1;
-  Dune::OneDGrid grid(10, 0, L);
+  const double EA = 1.0;
+  const int numElements = 10;
+  const int numGP = 2;
+  constexpr int polynomialOrder = 2;
+  Dune::OneDGrid grid(numElements, 0, L);
   //grid.globalRefine(2);
   auto gridView = grid.leafGridView();
   std::cout << gridView.dimensionworld << "\n";
   draw(gridView);
   using namespace Dune::Functions::BasisFactory;
-  auto basis = makeBasis(gridView, lagrange<1>());
-  std::vector<double> w(basis.size());
+  auto basis = makeBasis(gridView, lagrange<polynomialOrder>());
   auto basisView = basis.localView();
 
 
@@ -36,15 +37,67 @@ int main(){
     basisView.bind(ele);
     auto& fe = basisView.tree().finiteElement();
     Ikarus::LocalBasis localBasis(fe.localBasis());
-    const auto& rule = Dune::QuadratureRules<double, 1>::rule(ele.type(),2,Dune::QuadratureType::GaussLegendre);
-//    for (auto& gp : rule) {
-//      std::cout << gp.position() << "\n";
-//    }
-//    for (auto& gp : rule) {
-//      localBasis.evaluateFunctionAndJacobian()
-//
-//          energy
-//          += localBasis.
-//    }
+    const auto& rule = Dune::QuadratureRules<double, 1>::rule(ele.type(),numGP,Dune::QuadratureType::GaussLegendre);
+
+    Eigen::VectorXd dNdxi = Eigen::VectorXd::Zero(polynomialOrder+1);
+    Eigen::MatrixXd K = Eigen::MatrixXd::Zero(polynomialOrder+1,polynomialOrder+1);
+    Eigen::VectorXd B = Eigen::VectorXd::Zero(polynomialOrder+1);
+
+    auto detJ = ele.geometry().integrationElement(0.0);
+    for (auto& gp : rule) {
+      localBasis.evaluateJacobian(gp.position(),dNdxi);
+      B = dNdxi/detJ;
+      K += EA*B*B.transpose()*detJ*gp.weight();
+    }
+    std::cout << "The stiffness matrix is: " << "\n";
+    std::cout << K << "\n";
   }
+}
+
+
+void exampleTimoschenkoBeam(){
+  constexpr int griddim = 1;
+  const double L = 1;
+  const double EA = 1.0;
+  const int numElements = 10;
+  const int numGP = 2;
+  constexpr int polynomialOrderW = 1;
+  constexpr int polynomialOrderPhi = 1;
+  Dune::OneDGrid grid(numElements, 0, L);
+  //grid.globalRefine(2);
+  auto gridView = grid.leafGridView();
+  std::cout << gridView.dimensionworld << "\n";
+  draw(gridView);
+  using namespace Dune::Functions::BasisFactory;
+  // ToDo Basis mit unterschiedlichen Ordnungen
+  auto basis = makeBasis(gridView, power<1>(lagrange<1>(), FlatInterleaved()));
+  auto basisView = basis.localView();
+
+
+  for (auto& ele : elements(gridView)) {
+    basisView.bind(ele);
+    auto& fe = basisView.tree().finiteElement();
+    Ikarus::LocalBasis localBasis(fe.localBasis());
+    const auto& rule = Dune::QuadratureRules<double, 1>::rule(ele.type(),numGP,Dune::QuadratureType::GaussLegendre);
+
+    Eigen::VectorXd dNdxi = Eigen::VectorXd::Zero(polynomialOrder+1);
+    Eigen::MatrixXd K = Eigen::MatrixXd::Zero(polynomialOrder+1,polynomialOrder+1);
+    Eigen::VectorXd B = Eigen::VectorXd::Zero(polynomialOrder+1);
+
+    auto detJ = ele.geometry().integrationElement(0.0);
+    for (auto& gp : rule) {
+      localBasis.evaluateJacobian(gp.position(),dNdxi);
+      B = dNdxi/detJ;
+      K += EA*B*B.transpose()*detJ*gp.weight();
+    }
+    std::cout << "The stiffness matrix is: " << "\n";
+    std::cout << K << "\n";
+  }
+}
+
+
+
+int main(){
+  // exampleTrussElement();
+  exampleTimoschenkoBeam();
 }
