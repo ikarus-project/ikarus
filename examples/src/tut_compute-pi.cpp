b4955752a150517855588e8cae9ee5aebd9cabfd
AlexanderMueller <rathet@gmail.com>
2022-02-15 19:00:50 +0100

    compute pi correctly

--- examples/src/tut_compute-pi.cpp
+++ examples/src/tut_compute-pi.cpp
@@ -3,7 +3,7 @@
 //
 #include <../../config.h>
 #include <numbers>
-#define ALUGRIDDEBUG false
+
 #include <dune/alugrid/grid.hh>
 #include <dune/geometry/quadraturerules.hh>
 #include <dune/grid/common/boundarysegment.hh>
@@ -45,16 +45,14 @@ public:
   }
 };
 
-struct BS : Dune::BoundarySegment<2, 2, double> {
-
-  BS(const Dune::FieldVector<double, 2>& a ,const Dune::FieldVector<double, 2>& b) : corners{{a,b}} {}
-  virtual Dune::FieldVector<double, 2> operator()(const Dune::FieldVector<double, 1>& local) const override {
-    double angle =  + local[0] * (2 * std::numbers::pi - 0);
-
+struct UnitCircleBoundary : Dune::BoundarySegment<2, 2, double> {
+  UnitCircleBoundary(const Dune::FieldVector<double, 2>& a ,const Dune::FieldVector<double, 2>& b) : corners{{a,b}} {}
+  Dune::FieldVector<double, 2> operator()(const Dune::FieldVector<double, 1>& local) const override {
     Dune::FieldVector<double, 2> result = {0, 0};
-    result = corners[0] + local[0]*(corners[1]-corners[0]);
-
-    return result/result.two_norm();
+//    result = corners[0] + local[0]*(corners[1]-corners[0]);
+//    return result/result.two_norm();
+    double omega = std::acos(corners[0]*corners[1]);
+    return std::sin((1-local[0])*omega)/sin(omega)*corners[0]+ sin(local[0]*omega)/sin(omega)*corners[1];
   }
 
   std::array<Dune::FieldVector<double, 2>, 2> corners;
@@ -66,23 +64,43 @@ int main(int argc, char** argv) {
 
   using namespace Dune;
   constexpr int gridDim = 2;
-  Dune::GridFactory<Dune::ALUGrid<gridDim, 2, Dune::cube, Dune::nonconforming>> gridFactory;
+  Dune::GridFactory<Dune::ALUGrid<gridDim, 2, Dune::simplex, Dune::nonconforming>> gridFactory;
   const double h = 1.0;
   const double L = 1.0;
-  std::array<FieldVector<double, 2>, 4> corners0
-      = {{{-sqrt(2) / 2, -sqrt(2) / 2}, {sqrt(2) / 2, -sqrt(2) / 2}, {sqrt(2) / 2, sqrt(2) / 2}, {-sqrt(2) / 2, sqrt(2) / 2}}};
+//  std::array<FieldVector<double, 2>, 4> corners0 = {{{-sqrt(2) / 2, -sqrt(2) / 2}, {sqrt(2) / 2, -sqrt(2) / 2}, {sqrt(2) / 2, sqrt(2) / 2}, {-sqrt(2) / 2, sqrt(2) / 2}}};
+  Eigen::Vector2d v(1,0);
+  std::array<FieldVector<double, 2>, 6> corners0;
+  Eigen::Rotation2D<double> R;
+  R.angle() = 0.0;
+  for (int i = 0; i < corners0.size(); ++i) {
+   Eigen::Vector2d a = (R.toRotationMatrix()*v);
+   corners0[i][0]=a[0];
+   corners0[i][1]=a[1];
+   R.angle() += 60.0/180.0 * std::numbers::pi;
+  }
+
+  gridFactory.insertVertex({0,0});
   gridFactory.insertVertex(corners0[0]);
   gridFactory.insertVertex(corners0[1]);
   gridFactory.insertVertex(corners0[2]);
   gridFactory.insertVertex(corners0[3]);
+  gridFactory.insertVertex(corners0[4]);
+  gridFactory.insertVertex(corners0[5]);
 
   auto parametrization = [](const FieldVector<double, 2>& x) -> FieldVector<double, 2> { return x / x.two_norm(); };
 
-  gridFactory.insertElement(Dune::GeometryTypes::quadrilateral, {0, 1, 3,2});
-  gridFactory.insertBoundarySegment({0, 1}, std::make_shared<BS>(corners0[0],corners0[1]));
-  gridFactory.insertBoundarySegment({1, 2}, std::make_shared<BS>(corners0[1],corners0[2]));
-  gridFactory.insertBoundarySegment({0, 3}, std::make_shared<BS>(corners0[0],corners0[3]));
-  gridFactory.insertBoundarySegment({3, 2}, std::make_shared<BS>(corners0[3],corners0[2]));
+  gridFactory.insertElement(Dune::GeometryTypes::triangle, {0, 1, 2});
+  gridFactory.insertElement(Dune::GeometryTypes::triangle, {0, 2, 3});
+  gridFactory.insertElement(Dune::GeometryTypes::triangle, {0, 3, 4});
+  gridFactory.insertElement(Dune::GeometryTypes::triangle, {0, 4, 5});
+  gridFactory.insertElement(Dune::GeometryTypes::triangle, {0, 5, 6});
+  gridFactory.insertElement(Dune::GeometryTypes::triangle, {0, 6, 1});
+  gridFactory.insertBoundarySegment({1, 2}, std::make_shared<UnitCircleBoundary>(corners0[0],corners0[1]));
+  gridFactory.insertBoundarySegment({2, 3}, std::make_shared<UnitCircleBoundary>(corners0[1],corners0[2]));
+  gridFactory.insertBoundarySegment({3, 4}, std::make_shared<UnitCircleBoundary>(corners0[2],corners0[3]));
+  gridFactory.insertBoundarySegment({4, 5}, std::make_shared<UnitCircleBoundary>(corners0[3],corners0[4]));
+  gridFactory.insertBoundarySegment({5, 6}, std::make_shared<UnitCircleBoundary>(corners0[4],corners0[5]));
+  gridFactory.insertBoundarySegment({6, 1}, std::make_shared<UnitCircleBoundary>(corners0[5],corners0[0]));
 
 
   auto grid     = gridFactory.createGrid();
@@ -100,24 +118,28 @@ int main(int argc, char** argv) {
     const auto& rule = Dune::QuadratureRules<double, 2>::rule(element.type(), 10, Dune::QuadratureType::GaussLegendre);
     for (auto& gp : rule)
       area2 += element.geometry().integrationElement(gp.position()) * gp.weight();
-    //      area2 += f(element.geometry().global(gp.position())) * element.geometry().integrationElement(gp.position())
-    //               * gp.weight();  // integrationElement --> JacobiDeterminant
   }
 
   std::cout << "Area2 " << area2 << " " << std::numbers::pi << std::endl;
   bool adaptive = true;
   for (int i = 0; i < 10; ++i) {
     area1 = 0.0;
-    grid->globalRefine(1);
-
+//    grid->globalRefine(1);
+    for (auto ele :elements(grid->leafGridView())) {
+      if(ele.hasBoundaryIntersections())
+        grid->mark(1,ele);
+    }
+    grid->preAdapt();
+    grid->adapt();
+    grid->postAdapt();
     auto gridViewRefined = grid->leafGridView();
+
     std::cout << "This gridview contains: ";
     std::cout << gridViewRefined.size(0) << " elements" << std::endl;
 
-//    draw(gridViewRefined);
-    for (auto& element : elements(gridViewRefined)) {
+    for (auto& element : elements(gridViewRefined))
       area1 += element.geometry().volume();
-    }
+
     std::cout << "area1 " << area1 << " " << std::numbers::pi << std::endl;
     draw(gridViewRefined);
   }
@@ -128,6 +150,5 @@ int main(int argc, char** argv) {
       for (auto& intersection : intersections(grid->leafGridView(), element))
         if (intersection.boundary()) circumference += intersection.geometry().volume();
 
-  std::cout << circumference << std::endl;
-
+  std::cout << circumference /2<< " " << std::numbers::pi << std::endl;
 }
