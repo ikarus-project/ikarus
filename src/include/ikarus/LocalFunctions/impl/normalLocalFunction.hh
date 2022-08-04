c74521ae821b295dc4930008cb28fbc546342e76
RebeccaThierer <thierer@ibb.uni-stuttgart.de>
2022-08-04 17:14:11 +0200

    normalLocalFunction AM+RT

--- src/include/ikarus/LocalFunctions/impl/normalLocalFunction.hh
+++ src/include/ikarus/LocalFunctions/impl/normalLocalFunction.hh
@@ -35,18 +35,18 @@
 
 namespace Ikarus {
 
-  template <typename DuneBasis, typename CoeffContainer, std::size_t ID = 0>
-  class StandardLocalFunction : public LocalFunctionInterface<StandardLocalFunction<DuneBasis, CoeffContainer, ID>>,
-                                public ClonableLocalFunction<StandardLocalFunction<DuneBasis, CoeffContainer, ID>> {
-    using Interface = LocalFunctionInterface<StandardLocalFunction<DuneBasis, CoeffContainer, ID>>;
+  template <typename Geometry, typename DuneBasis, typename CoeffContainer, std::size_t ID = 0>
+  class NormalLocalFunction : public LocalFunctionInterface<NormalLocalFunction<Geometry,DuneBasis, CoeffContainer, ID>>,
+                                public ClonableLocalFunction<NormalLocalFunction<Geometry,DuneBasis, CoeffContainer, ID>> {
+    using Interface = LocalFunctionInterface<NormalLocalFunction<Geometry,DuneBasis, CoeffContainer, ID>>;
 
   public:
     friend Interface;
-    friend ClonableLocalFunction<StandardLocalFunction>;
+    friend ClonableLocalFunction<NormalLocalFunction>;
 
-    constexpr StandardLocalFunction(const Ikarus::LocalBasis<DuneBasis>& p_basis, const CoeffContainer& coeffs_,
+    constexpr NormalLocalFunction(const Geometry& geo,const Ikarus::LocalBasis<DuneBasis>& p_basis, const CoeffContainer& coeffs_,
                                     Dune::template index_constant<ID> = Dune::template index_constant<std::size_t(0)>{})
-        : basis_{p_basis}, coeffs{coeffs_}, coeffsAsMat{Ikarus::viewAsEigenMatrixFixedDyn(coeffs)} {}
+        : geo_{geo},basis_{p_basis}, coeffs{coeffs_}, coeffsAsMat{Ikarus::viewAsEigenMatrixFixedDyn(coeffs)} {}
 
     static constexpr bool isLeaf = true;
     using Ids                    = Dune::index_constant<ID>;
@@ -62,7 +62,7 @@ namespace Ikarus {
     friend auto evaluateFunctionImpl(const LocalFunctionInterface<LocalFunctionImpl_>& f,
                                      const LocalFunctionEvaluationArgs_& localFunctionArgs);
 
-    using Traits = LocalFunctionTraits<StandardLocalFunction<DuneBasis, CoeffContainer, ID>>;
+    using Traits = LocalFunctionTraits<NormalLocalFunction<Geometry,DuneBasis, CoeffContainer, ID>>;
     /** \brief Type used for coordinates */
     using ctype = typename Traits::ctype;
     //    /** \brief Dimension of the coeffs */
@@ -93,8 +93,8 @@ namespace Ikarus {
 
     template <typename OtherType>
     struct Rebind {
-      using other = StandardLocalFunction<
-          DuneBasis, typename Std::Rebind<CoeffContainer, typename Manifold::template Rebind<OtherType>::other>::other,
+      using other = NormalLocalFunction<
+          Geometry,DuneBasis, typename Std::Rebind<CoeffContainer, typename Manifold::template Rebind<OtherType>::other>::other,
           ID>;
     };
 
@@ -103,21 +103,44 @@ namespace Ikarus {
   private:
     template <typename DomainTypeOrIntegrationPointIndex, typename... TransformArgs>
     FunctionReturnType evaluateFunctionImpl(const DomainTypeOrIntegrationPointIndex& ipIndexOrPosition,
-                                            [[maybe_unused]] const TransformWith<TransformArgs...>&) const {
-      const auto& N = evaluateFunctionWithIPorCoord(ipIndexOrPosition, basis_);
+                                            [[maybe_unused]] const TransformWith<TransformArgs...>& transArgs) const {
+      const auto& dNraw = evaluateDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
+      maytransformDerivatives(dNraw, dNTransformed, transArgs);
+      auto a_1a_2 =  (toEigenMatrix(geo_.jacobianTransposed(ipIndexOrPosition))+ coeffsAsMat * dNTransformed.template cast<ctype>()).eval();
+      //     [a_1 a_2]  = [A_1 A_2]                     +                   [u_1 u_2]
 
-      return FunctionReturnType(coeffsAsMat * N);
+      return a_1a_2.col(0).cross(a_1a_2.col(1));
     }
 
     template <typename DomainTypeOrIntegrationPointIndex, typename... TransformArgs>
     Jacobian evaluateDerivativeWRTSpaceAllImpl(const DomainTypeOrIntegrationPointIndex& ipIndexOrPosition,
                                                const TransformWith<TransformArgs...>& transArgs) const {
+      // get Second Derivatives of Ansatz Function
       const auto& dNraw = evaluateDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
       maytransformDerivatives(dNraw, dNTransformed, transArgs);
-      return coeffsAsMat
-             * dNTransformed
-                   .template cast<ctype>();  // The cast here is only necessary since the autodiff types are not working
-                                             // otherwise, see Issue https://github.com/autodiff/autodiff/issues/73
+      auto a_1a_2 =  (toEigenMatrix(geo_.jacobianTransposed(ipIndexOrPosition))+ coeffsAsMat * dNTransformed.template cast<ctype>()).eval();
+      auto a1 =  a_1a_2.col(0).eval();
+      auto a2 =  a_1a_2.col(1).eval();
+     auto A_11A_22_A_12 =  toEigenMatrix(geo_.secondDerivativeOfPosition(ipIndexOrPosition));
+     auto A_11 =  A_11A_22_A_12.col(0).eval();
+     auto A_22 =  A_11A_22_A_12.col(1).eval();
+     auto A_12 =  A_11A_22_A_12.col(2).eval();
+     const auto& ddNraw = evaluateSecondDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
+     static_assert(transArgs.isEmpty,"Transform second derivatives not implemented.");
+
+     auto u_11u_22_u12 = coeffsAsMat * ddNraw.template cast<ctype>();
+     auto u_11 =  u_11u_22_u12.col(0).eval();
+     auto u_22 = u_11u_22_u12.col(1).eval();
+     auto u_12 =   u_11u_22_u12.col(2).eval();
+
+     auto a_11 =  (A_11+u_11).eval();
+     auto a_22 =  (A_22+u_22).eval();
+     auto a_12 =  (A_12+u_12).eval();
+     Jacobian res;
+     res.col(0) = a_11.cross(a2) + a1.cross(a_12);
+     res.col(1) = a_12.cross(a2) + a1.cross(a_22);
+
+     return res;
     }
 
     template <typename DomainTypeOrIntegrationPointIndex, typename... TransformArgs>
@@ -126,19 +149,37 @@ namespace Ikarus {
                                                          const TransformWith<TransformArgs...>& transArgs) const {
       const auto& dNraw = evaluateDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
       maytransformDerivatives(dNraw, dNTransformed, transArgs);
+      auto a_1a_2 =  (toEigenMatrix(geo_.jacobianTransposed(ipIndexOrPosition))+ coeffsAsMat * dNTransformed.template cast<ctype>()).eval();
 
-      return coeffsAsMat * dNTransformed.col(spaceIndex);
+      auto A_11A_22_A_12 =  toEigenMatrix(geo_.secondDerivativeOfPosition(ipIndexOrPosition));
+      const auto& ddNraw = evaluateSecondDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
+      static_assert(transArgs.isEmpty,"Transform second derivatives not implemented.");
+
+      auto u_11u_22_u12 = coeffsAsMat * ddNraw.template cast<ctype>();
+
+      auto a_11a_22_a_12 = A_11A_22_A_12+u_11u_22_u12;
+      const int otherIndex = spaceIndex==0? 1: 0;
+      Jacobian res;
+      //  a_11a_22_a_12[0].cross(a2) + a1.cross(a_11a_22_a_12[2]);
+      // -a_11a_22_a_12[1].cross(a1) - a2.cross(a_11a_22_a_12[2]) ;
+      return (a_11a_22_a_12[spaceIndex].cross(a_1a_2.col(otherIndex)) + a_1a_2.col(spaceIndex).cross(a_11a_22_a_12[2]))* (spaceIndex==0? 1: -1);
+
     }
 
     template <typename DomainTypeOrIntegrationPointIndex, typename... TransformArgs>
     CoeffDerivMatrix evaluateDerivativeWRTCoeffsImpl(const DomainTypeOrIntegrationPointIndex& ipIndexOrPosition,
                                                      int coeffsIndex,
                                                      const TransformWith<TransformArgs...>& transArgs) const {
-      const auto& N = evaluateFunctionWithIPorCoord(ipIndexOrPosition, basis_);
-      CoeffDerivMatrix mat;
-      mat.setIdentity(valueSize);
-      mat.diagonal() *= N[coeffsIndex];
-      return mat;
+      const auto& dNraw = evaluateDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
+      maytransformDerivatives(dNraw, dNTransformed, transArgs);
+      auto a_1a_2 =  (toEigenMatrix(geo_.jacobianTransposed(ipIndexOrPosition))+ coeffsAsMat * dNTransformed.template cast<ctype>()).eval();
+      //     [a_1 a_2]  = [A_1 A_2]                     +                   [u_1 u_2]
+      Eigen::DiagonalMatrix<ctype,3> a1dC = Eigen::Matrix<ctype,3,3>::Identity()* dNTransformed(coeffsIndex,0);
+      Eigen::DiagonalMatrix<ctype,3> a2dC = Eigen::Matrix<ctype,3,3>::Identity()* dNTransformed(coeffsIndex,1);
+      Eigen::Matrix<ctype,3,3> a1Hat = toSkewMatrix(a_1a_2.col(0));
+      Eigen::Matrix<ctype,3,3> a2Hat = toSkewMatrix(a_1a_2.col(1));
+
+      return a1Hat*a2dC-a2Hat*a1dC;
     }
 
     template <typename DomainTypeOrIntegrationPointIndex, typename... TransformArgs>
@@ -146,14 +187,38 @@ namespace Ikarus {
         const DomainTypeOrIntegrationPointIndex& ipIndexOrPosition, int coeffsIndex,
         const TransformWith<TransformArgs...>& transArgs) const {
       const auto& dNraw = evaluateDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
+      const auto& ddNraw = evaluateSecondDerivativeWithIPorCoord(ipIndexOrPosition, basis_);
+      static_assert(transArgs.isEmpty,"Transform second derivatives not implemented.");
       maytransformDerivatives(dNraw, dNTransformed, transArgs);
-      std::array<CoeffDerivMatrix, gridDim> Warray;
-      for (int dir = 0; dir < gridDim; ++dir) {
-        Warray[dir].setIdentity(valueSize);
-        Warray[dir].diagonal() *= dNTransformed(coeffsIndex, dir);
-      }
+      auto a_1a_2 =  (toEigenMatrix(geo_.jacobianTransposed(ipIndexOrPosition))+ coeffsAsMat * dNTransformed.template cast<ctype>()).eval();
+      //     [a_1 a_2]  = [A_1 A_2]                     +                   [u_1 u_2]
+      Eigen::DiagonalMatrix<ctype,3> a1_C = Eigen::Matrix<ctype,3,3>::Identity()* dNTransformed(coeffsIndex,0);
+      Eigen::DiagonalMatrix<ctype,3> a2_C = Eigen::Matrix<ctype,3,3>::Identity()* dNTransformed(coeffsIndex,1);
 
-      return Warray;
+      // get Second Derivatives of Ansatz Function
+      auto a1 =  a_1a_2.col(0).eval();
+      auto a2 =  a_1a_2.col(1).eval();
+      auto A_11A_22_A_12 =  toEigenMatrix(geo_.secondDerivativeOfPosition(ipIndexOrPosition));
+      auto A_11 =  A_11A_22_A_12.col(0).eval();
+      auto A_22 =  A_11A_22_A_12.col(1).eval();
+      auto A_12 =  A_11A_22_A_12.col(2).eval();
+
+      auto u_11u_22_u12 = coeffsAsMat * ddNraw.template cast<ctype>();
+      auto u_11 =  u_11u_22_u12.col(0).eval();
+      auto u_22 = u_11u_22_u12.col(1).eval();
+      auto u_12 =   u_11u_22_u12.col(2).eval();
+
+      auto a_11 =  (A_11+u_11).eval();
+      auto a_22 =  (A_22+u_22).eval();
+      auto a_12 =  (A_12+u_12).eval();
+      Eigen::DiagonalMatrix<ctype,3> a_11_C = Eigen::Matrix<ctype,3,3>::Identity()* ddNraw(coeffsIndex,0);
+      Eigen::DiagonalMatrix<ctype,3> a_22_C = Eigen::Matrix<ctype,3,3>::Identity()* ddNraw(coeffsIndex,1);
+      Eigen::DiagonalMatrix<ctype,3> a_12_C = Eigen::Matrix<ctype,3,3>::Identity()* ddNraw(coeffsIndex,2);
+      std::array<CoeffDerivMatrix, 2> a3_1_C_a3_2_C;
+      a3_1_C_a3_2_C[0] = -cross(a2,a_11_C) + cross(a_11,a2_C) -cross(a_12,a1_C) + cross(a1,a_12_C);
+      a3_1_C_a3_2_C[1] = -cross(a2,a_12_C) + cross(a_12,a2_C) -cross(a_22,a1_C) + cross(a1,a_22_C);
+
+      return a3_1_C_a3_2_C;
     }
 
     template <typename DomainTypeOrIntegrationPointIndex, typename... TransformArgs>
@@ -171,12 +236,13 @@ namespace Ikarus {
 
     mutable AnsatzFunctionJacobian dNTransformed;
     const Ikarus::LocalBasis<DuneBasis>& basis_;
+    const Geometry& geo_;
     CoeffContainer coeffs;
     const decltype(Ikarus::viewAsEigenMatrixFixedDyn(coeffs)) coeffsAsMat;
   };
 
-  template <typename DuneBasis, typename CoeffContainer, std::size_t ID>
-  struct LocalFunctionTraits<StandardLocalFunction<DuneBasis, CoeffContainer, ID>> {
+  template <typename Geometry,typename DuneBasis, typename CoeffContainer, std::size_t ID>
+  struct LocalFunctionTraits<NormalLocalFunction<Geometry,DuneBasis, CoeffContainer, ID>> {
     /** \brief Type used for coordinates */
     using ctype = typename CoeffContainer::value_type::ctype;
     /** \brief Dimension of the coeffs */
