f23d45290f7b69cfa5bf6ee9747f3534ee53d00e
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2023-01-04 09:34:52 +0100

    first compiling version

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -6,9 +6,24 @@
 #include <iostream>
 
 #include <dune/common/parallel/mpihelper.hh>
+#include <clipper2/clipper.h>
+
 
 int main(int argc, char** argv) {
+  using namespace Clipper2Lib;
   Dune::MPIHelper::instance(argc, argv);
 
-  std::cout << "Hello from ikarus!" << std::endl;
+  //set up the subject and clip polygons ...
+  Paths64 subject;
+  subject.push_back(Ellipse(Rect64(100,100,300,300)));
+  subject.push_back(Ellipse(Rect64(125,130,275,180)));
+  subject.push_back(Ellipse(Rect64(125,220,275,270)));
+
+  Paths64 clip;
+  clip.push_back(Ellipse(Rect64(140,70,220,320)));
+
+  //get the intersection
+  Paths64 solution = Intersect(subject, clip, FillRule::EvenOdd);
+
+  //DrawPolygons
 }
