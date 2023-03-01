f361eeb3044b6fde9c7348acd0ce3e19c0d236cc
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2023-03-01 17:37:35 +0100

    added earcut

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -4,6 +4,7 @@
 #include <config.h>
 
 #include <clipper2/clipper.h>
+#include <mapbox/earcut.hpp>
 #include <iostream>
 #include <matplot/matplot.h>
 
@@ -117,4 +118,28 @@ int main(int argc, char** argv) {
   drawPaths(clip2, "red");
   drawPaths(trimmedandEmptyElements, "blue");
   f2->show();
+
+
+  // The number type to use for tessellation
+  using Coord = double;
+
+// The index type. Defaults to uint32_t, but you can also pass uint16_t if you know that your
+// data won't have more than 65536 vertices.
+  using N = uint32_t;
+
+// Create array
+  using Point = std::array<Coord, 2>;
+  std::vector<std::vector<Point>> polygon;
+
+// Fill polygon structure with actual data. Any winding order works.
+// The first polyline defines the main polygon.
+  polygon.push_back({{100, 0}, {100, 100}, {0, 100}, {0, 0}});
+// Following polylines define holes.
+  polygon.push_back({{75, 25}, {75, 75}, {25, 75}, {25, 25}});
+
+// Run tessellation
+// Returns array of indices that refer to the vertices of the input polygon.
+// e.g: the index 6 would refer to {25, 75} in this example.
+// Three subsequent indices form a triangle. Output triangles are clockwise.
+  std::vector<N> indices = mapbox::earcut<N>(polygon);
 }
