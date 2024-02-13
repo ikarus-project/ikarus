a8e2331c960820e022c916a46f7018d1dd7328e7
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2024-02-13 09:21:56 +0100

    Initial commit

---
+++ ikarus/finiteelements/fesettings.hh
@@ -0,0 +1,8 @@
+//
+// Created by ac136110 on 13.02.2024.
+//
+
+#ifndef FESETTINGS_HH
+#define FESETTINGS_HH
+
+#endif //FESETTINGS_HH
---
+++ ikarus/io/loadfesettings.hh
@@ -0,0 +1,4 @@
+// SPDX-FileCopyrightText: 2021-2024 The Ikarus Developers mueller@ibb.uni-stuttgart.de
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
