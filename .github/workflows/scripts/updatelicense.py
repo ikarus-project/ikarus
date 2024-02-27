f10add6bdbd2947e5d0e9eb8a058683ff13698e5
Henrik Jakob <96132706+henrij22@users.noreply.github.com>
2024-02-27 11:07:09 +0100

    Fix workflow + license (#265)

--- .github/workflows/scripts/updatelicense.py
+++ .github/workflows/scripts/updatelicense.py
@@ -28,11 +28,32 @@ def update_license_header(file_path):
         print(f"Error updating license header in {file_path}: {e}")
 
 
+def update_dep5(file_path):
+    try:
+        with open(file_path, "r", newline="") as file:
+            content = file.read()
+
+        updated_content, nsubs = re.subn(
+            r"Copyright: (\d{4}-\d{4}) The Ikarus Developers",
+            lambda match: f"Copyright: 2021-{datetime.datetime.now().year} The Ikarus Developers",
+            content,
+        )
+        if nsubs > 0:
+            with open(file_path, "w", newline="") as file:
+                file.write(updated_content)
+
+    except Exception as e:
+        print(f"Error updating license information in {file_path}: {e}")
+
+
 def process_directory(directory):
     for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
-            update_license_header(file_path)
+            if os.path.basename(file_path) == 'dep5':
+                update_dep5(file_path)
+            else:
+                update_license_header(file_path)
 
 
 if __name__ == "__main__":
