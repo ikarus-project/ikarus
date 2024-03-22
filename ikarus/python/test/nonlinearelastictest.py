47a27c8559b3dc9bde1d17fc78e13bd38a27a2bd
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2024-03-22 13:01:34 +0100

    all tests

--- ikarus/python/test/nonlinearelastictest.py
+++ ikarus/python/test/nonlinearelastictest.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: 2021-2024 The Ikarus Developers mueller@ibb.uni-stuttgart.de
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-import setpath
+# import setpath
 
-setpath.set_path()
+# setpath.set_path()
 import ikarus as iks
 import ikarus.finite_elements
 import ikarus.utils
