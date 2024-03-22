a9ea0186e2e2e5830cfeaaa71135355d2d62bf5a
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2024-03-22 12:01:27 +0100

    readd

--- ikarus/python/test/linearelastictest.py
+++ ikarus/python/test/linearelastictest.py
@@ -1,10 +1,10 @@
 # SPDX-FileCopyrightText: 2021-2024 The Ikarus Developers mueller@ibb.uni-stuttgart.de
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-import setpath
+# import setpath
 import os
 
-setpath.set_path()
+#setpath.set_path()
 import ikarus as iks
 import ikarus.finite_elements
 import ikarus.utils
