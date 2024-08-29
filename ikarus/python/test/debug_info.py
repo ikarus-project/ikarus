cdbaafb1863a75e2cb98e7fb75820a26652f265f
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2024-08-29 11:27:16 +0200

    info again
    I cant get it to display anything in ctest environment

--- ikarus/python/test/debug_info.py
+++ ikarus/python/test/debug_info.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import os
 import dune.generator as generator
-from logging import warn
+from logging import info
 
 os.environ["DUNE_LOG_LEVEL"] = "debug"
 os.environ["DUNE_SAVE_BUILD"] = "terminal"
@@ -18,9 +18,9 @@ def setDebugFlags():
     # Apply flags only if the build type is Debug, or if the variable is not set
     if build_type == "Debug" or build_type is None:
         apply_debug_flags()
-        warn("JIT Python Bindings BUILD_TYPE: Debug")
+        info("JIT Python Bindings BUILD_TYPE: Debug")
     else:
-        warn("JIT Python Bindings BUILD_TYPE: Release")
+        info("JIT Python Bindings BUILD_TYPE: Release")
 
 
 def unsetDebugFlags():
