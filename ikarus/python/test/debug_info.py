7f9627233526f823ed863dc43d9c1a4ea24ef35d
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2024-08-29 11:17:34 +0200

    set to debug

--- ikarus/python/test/debug_info.py
+++ ikarus/python/test/debug_info.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 import os
 import dune.generator as generator
-from logging import info
+from logging import debug
 
 os.environ["DUNE_LOG_LEVEL"] = "debug"
 os.environ["DUNE_SAVE_BUILD"] = "terminal"
@@ -18,9 +18,9 @@ def setDebugFlags():
     # Apply flags only if the build type is Debug, or if the variable is not set
     if build_type == "Debug" or build_type is None:
         apply_debug_flags()
-        info("JIT Python Bindings BUILD_TYPE: Debug")
+        debug("JIT Python Bindings BUILD_TYPE: Debug")
     else:
-        info("JIT Python Bindings BUILD_TYPE: Release")
+        debug("JIT Python Bindings BUILD_TYPE: Release")
 
 
 def unsetDebugFlags():
