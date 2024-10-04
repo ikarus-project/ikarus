aef902c8849761042fa0463dc86dc38286ed3b80
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2024-10-04 08:57:59 +0200

    refactor autodiff test

--- tests/src/checkfebyautodiff.hh
+++ tests/src/checkfebyautodiff.hh
@@ -16,15 +16,14 @@
 
 template <typename GridView, typename BasisHandler, typename Skills, typename AffordanceColl, typename VectorType>
 auto checkFESByAutoDiffImpl(const GridView& gridView, const BasisHandler& basis, Skills&& skills,
-                            AffordanceColl affordance, VectorType& d, const std::string& messageIfFailed = "") {
+                            AffordanceColl affordance, VectorType& d, const std::string& testName = "") {
   double lambda = 7.3;
   auto fe       = Ikarus::makeFE(basis, std::forward<Skills>(skills));
   using FE      = decltype(fe);
 
   auto req = typename FE::Requirement();
   req.insertGlobalSolution(d).insertParameter(lambda);
-  Dune::TestSuite t("Check calculateScalarImpl() and calculateVectorImpl() by Automatic Differentiation" +
-                    messageIfFailed);
+  Dune::TestSuite t("Check calculateScalarImpl() and calculateVectorImpl() by Automatic Differentiation" + testName);
   for (auto element : elements(gridView)) {
     auto localView = basis.flat().localView();
     localView.bind(element);
@@ -32,6 +31,7 @@ auto checkFESByAutoDiffImpl(const GridView& gridView, const BasisHandler& basis,
     const double tol = 1e-10;
 
     fe.bind(element);
+    updateState(fe, req, d); // here d = correction vector (DeltaD)
 
     const std::string feClassName = Dune::className(fe);
 
@@ -42,15 +42,8 @@ auto checkFESByAutoDiffImpl(const GridView& gridView, const BasisHandler& basis,
     K.setZero(nDOF, nDOF);
     KAutoDiff.setZero(nDOF, nDOF);
 
-    Eigen::VectorXd R, RAutoDiff;
-    R.setZero(nDOF);
-    RAutoDiff.setZero(nDOF);
-
     calculateMatrix(fe, req, affordance.matrixAffordance(), K);
     calculateMatrix(feAutoDiff, req, affordance.matrixAffordance(), KAutoDiff);
-
-    calculateVector(fe, req, affordance.vectorAffordance(), R);
-    calculateVector(feAutoDiff, req, affordance.vectorAffordance(), RAutoDiff);
 
     t.check(isApproxSame(K, KAutoDiff, tol),
             "Mismatch between the stiffness matrices obtained from explicit implementation and the one based on "
@@ -61,18 +54,50 @@ auto checkFESByAutoDiffImpl(const GridView& gridView, const BasisHandler& basis,
         << KAutoDiff << "\nThe difference is\n"
         << (K - KAutoDiff);
 
-    t.check(isApproxSame(R, RAutoDiff, tol),
-            "Mismatch between the residual vectors obtained from explicit implementation and the one based on "
-            "automatic differentiation for " +
-                feClassName)
-        << "R is " << R.transpose() << "\nRAutoDiff is " << RAutoDiff.transpose() << "\nThe difference is "
-        << (R - RAutoDiff).transpose();
+    if constexpr (requires { fe.numberOfEASParameters(); }) {
+      t.check(fe.numberOfEASParameters() == feAutoDiff.realFE().numberOfEASParameters())
+          << "Number of EAS parameters for FE(" << fe.numberOfEASParameters()
+          << ") and number of EAS parameters for AutodiffFE(" << feAutoDiff.realFE().numberOfEASParameters()
+          << ") are not equal";
 
-    t.check(Dune::FloatCmp::eq(calculateScalar(fe, req, affordance.scalarAffordance()),
-                               calculateScalar(feAutoDiff, req, affordance.scalarAffordance()), tol),
-            "Mismatch between the energies obtained from explicit implementation and the one based on "
-            "automatic differentiation for " +
-                feClassName);
+      t.check(fe.alpha().isApprox(feAutoDiff.alpha(), tol), "Mismatch in alpha.")
+          << "alpha is\n"
+          << fe.alpha().transpose() << "\n alphaAutoDiff is\n"
+          << feAutoDiff.alpha();
+
+      if (fe.numberOfEASParameters() != 0) {
+        t.checkThrow<Dune::NotImplemented>(
+            [&]() {
+              Eigen::VectorXd RAutoDiff;
+              RAutoDiff.setZero(nDOF);
+              calculateVector(feAutoDiff, req, affordance.vectorAffordance(), RAutoDiff);
+            },
+            "calculateVector with AutoDiff should throw a Dune::NotImplemented");
+
+        t.checkThrow<Dune::NotImplemented>(
+            [&]() { auto energyAutoDiff = calculateScalar(feAutoDiff, req, affordance.scalarAffordance()); },
+            "calculateScalar with AutoDiff should throw a Dune::NotImplemented");
+      }
+    } else {
+      Eigen::VectorXd R, RAutoDiff;
+      R.setZero(nDOF);
+      RAutoDiff.setZero(nDOF);
+
+      calculateVector(fe, req, affordance.vectorAffordance(), R);
+      calculateVector(feAutoDiff, req, affordance.vectorAffordance(), RAutoDiff);
+      t.check(isApproxSame(R, RAutoDiff, tol),
+              "Mismatch between the residual vectors obtained from explicit implementation and the one based on "
+              "automatic differentiation for " +
+                  feClassName)
+          << "R is " << R.transpose() << "\nRAutoDiff is " << RAutoDiff.transpose() << "\nThe difference is "
+          << (R - RAutoDiff).transpose();
+
+      t.check(Dune::FloatCmp::eq(calculateScalar(fe, req, affordance.scalarAffordance()),
+                                 calculateScalar(feAutoDiff, req, affordance.scalarAffordance()), tol),
+              "Mismatch between the energies obtained from explicit implementation and the one based on "
+              "automatic differentiation for " +
+                  feClassName);
+    }
   }
 
   return t;
--- tests/src/testnonlineareas.cpp
+++ tests/src/testnonlineareas.cpp
@@ -3,6 +3,7 @@
 
 #include <config.h>
 
+#include "checkfebyautodiff.hh"
 #include "testcommon.hh"
 
 #include <dune/common/test/testsuite.hh>
@@ -22,78 +23,8 @@ using namespace Ikarus;
 using namespace Dune::Indices;
 using Dune::TestSuite;
 
-template <int gridDim, typename GV, typename MAT, typename BH, typename VectorType>
-auto nonlinearEASAutoDiffTest(const GV& gridView, const MAT& mat, const BH& basis, VectorType& d,
-                              const int numberOfEASParameters) {
-  TestSuite t("Nonlinear EAS AutoDiff with gridDim = " + std::to_string(gridDim) +
-              " and numberOfEASParameters = " + std::to_string(numberOfEASParameters));
-  double lambda = 7.3;
-  auto sk       = skills(nonLinearElastic(mat), eas(numberOfEASParameters));
-  auto fe       = Ikarus::makeFE(basis, sk);
-  using FE      = decltype(fe);
-  auto req      = typename FE::Requirement();
-  req.insertGlobalSolution(d).insertParameter(lambda);
-
-  auto affordance = AffordanceCollections::elastoStatics;
-
-  for (auto element : elements(gridView)) {
-    auto localView = basis.flat().localView();
-    localView.bind(element);
-    auto nDOF        = localView.size();
-    const double tol = 1e-10;
-
-    fe.bind(element);
-    updateState(fe, req, d); // here d = correction vector (DeltaD)
-
-    const std::string feClassName = Dune::className(fe);
-
-    using AutoDiffBasedFE = Ikarus::AutoDiffFE<decltype(fe), true>;
-    AutoDiffBasedFE feAutoDiff(fe);
-
-    Eigen::MatrixXd K, KAutoDiff;
-    K.setZero(nDOF, nDOF);
-    KAutoDiff.setZero(nDOF, nDOF);
-
-    calculateMatrix(fe, req, affordance.matrixAffordance(), K);
-    calculateMatrix(feAutoDiff, req, affordance.matrixAffordance(), KAutoDiff);
-
-    t.check(fe.numberOfEASParameters() == feAutoDiff.realFE().numberOfEASParameters())
-        << "Number of EAS parameters for FE(" << fe.numberOfEASParameters()
-        << ") and number of EAS parameters for AutodiffFE(" << feAutoDiff.realFE().numberOfEASParameters()
-        << ") are not equal";
-
-    t.check(fe.alpha().isApprox(feAutoDiff.alpha(), tol), "Mismatch in alpha.")
-        << "alpha is\n"
-        << fe.alpha().transpose() << "\n alphaAutoDiff is\n"
-        << feAutoDiff.alpha();
-
-    t.check(K.isApprox(KAutoDiff, tol),
-            "Mismatch between the stiffness matrices obtained from explicit implementation and the one based on "
-            "automatic differentiation.")
-        << "K is \n"
-        << K << "\nKAutoDiff is \n"
-        << KAutoDiff << "\nThe difference is\n"
-        << (K - KAutoDiff);
-
-    if (numberOfEASParameters != 0) {
-      t.checkThrow<Dune::NotImplemented>(
-          [&]() {
-            Eigen::VectorXd RAutoDiff;
-            RAutoDiff.setZero(nDOF);
-            calculateVector(feAutoDiff, req, affordance.vectorAffordance(), RAutoDiff);
-          },
-          "calculateVector with AutoDiff should throw a Dune::NotImplemented");
-
-      t.checkThrow<Dune::NotImplemented>(
-          [&]() { auto energyAutoDiff = calculateScalar(feAutoDiff, req, affordance.scalarAffordance()); },
-          "calculateScalar with AutoDiff should throw a Dune::NotImplemented");
-    }
-  }
-  return t;
-}
-
-template <int gridDim, typename TestSuitType>
-void easAutoDiffTest(const TestSuitType& t) {
+template <int gridDim, typename TestSuitType, typename MAT>
+void easAutoDiffTest(const TestSuitType& t, const MAT& mat) {
   std::array<int, (gridDim == 2) ? 4 : 3> easParameters;
   if constexpr (gridDim == 2)
     easParameters = {0, 4, 5, 7};
@@ -102,34 +33,25 @@ void easAutoDiffTest(const TestSuitType& t) {
 
   auto grid = createUGGridFromCorners<gridDim>(CornerDistortionFlag::randomlyDistorted);
   grid->globalRefine(2);
-  auto gridView     = grid->leafGridView();
-  auto matParameter = toLamesFirstParameterAndShearModulus({.emodul = 100.0, .nu = 0.3});
-  StVenantKirchhoff matSVK(matParameter);
-  auto reducedMat = planeStress(matSVK);
+  auto gridView = grid->leafGridView();
 
   using namespace Dune::Functions::BasisFactory;
   auto basis = Ikarus::makeBasis(gridView, power<gridDim>(lagrange<1>()));
-  Eigen::VectorXd d;
-
-  auto autoDiffTestFunctor = [&](int numberOfEASParameters) {
-    if constexpr (gridDim == 2)
-      nonlinearEASAutoDiffTest<gridDim>(gridView, reducedMat, basis, d, numberOfEASParameters);
-    else
-      nonlinearEASAutoDiffTest<gridDim>(gridView, matSVK, basis, d, numberOfEASParameters);
-  };
 
   for (const int numberOfEASParameters : easParameters) {
-    d.setZero(basis.flat().size());
-    autoDiffTestFunctor(numberOfEASParameters);
-    d.setRandom(basis.flat().size());
-    autoDiffTestFunctor(numberOfEASParameters);
+    auto sk = skills(nonLinearElastic(mat), eas(numberOfEASParameters));
+    checkFESByAutoDiff(gridView, power<gridDim>(lagrange<1>()), sk, AffordanceCollections::elastoStatics,
+                       " (numberOfEASParameters = " + std::to_string(numberOfEASParameters) + ")");
   }
 }
 
 int main(int argc, char** argv) {
   Ikarus::init(argc, argv);
   Dune::TestSuite t("Nonlinear EAS Test");
-  easAutoDiffTest<2>(t);
-  easAutoDiffTest<3>(t);
+  auto matParameter = toLamesFirstParameterAndShearModulus({.emodul = 100.0, .nu = 0.3});
+  StVenantKirchhoff matSVK(matParameter);
+  auto reducedMat = planeStress(matSVK);
+  easAutoDiffTest<2>(t, reducedMat);
+  easAutoDiffTest<3>(t, matSVK);
   return t.exit();
 }
