e077670c928959269cf3075b27ea43c7edd4aec9
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2024-10-29 11:32:07 +0100

    Bump Dune Version in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from skbuild import setup
 # /dune/dune-common/build-cmake/run-in-dune-env python -m twine upload dist/* --verbose
 
 ikarusVersion = "0.4.1"
-duneVersion = "2.10.0"
+duneVersion = "2.11.0"
 
 metadata = metaData(duneVersion)[1]
 metadata["version"] = ikarusVersion
