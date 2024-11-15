ac2b0638b88897d69a8478bae37538200643cd69
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2024-11-15 14:08:36 +0100

    add tangentModuli for general InvariantBased model

--- ikarus/finiteelements/mechanics/materials/hyperelastic/deviatoric/invariantbased.hh
+++ ikarus/finiteelements/mechanics/materials/hyperelastic/deviatoric/invariantbased.hh
@@ -15,6 +15,82 @@
 
 namespace Ikarus::Materials {
 
+namespace Impl {
+  template <typename PS>
+  struct DeviatoricInvariants
+  {
+    using PrincipalStretches = PS;
+    using ScalarType         = PrincipalStretches::value_type;
+    static constexpr int dim = PrincipalStretches::RowsAtCompileTime;
+    using Invariants         = PrincipalStretches;
+
+    using FirstDerivative  = Eigen::Vector<ScalarType, dim>;
+    using SecondDerivative = Eigen::Matrix<ScalarType, dim, dim>;
+
+    explicit DeviatoricInvariants(const PrincipalStretches& lambda)
+        : lambda_{lambda} {}
+
+    auto value() const {
+      const Invariants& invariants = Impl::invariants(lambda_);
+      ScalarType W1                = invariants[0] * pow(invariants[2], -1.0 / 3.0) - 3.0;
+      ScalarType W2                = invariants[1] * pow(invariants[2], -2.0 / 3.0) - 3.0;
+      return std::make_pair(W1, W2);
+    }
+
+    auto firstDerivative() const {
+      const Invariants& invariants            = Impl::invariants(lambda_);
+      auto dW1dLambda                         = FirstDerivative::Zero().eval();
+      auto dW2dLambda                         = FirstDerivative::Zero().eval();
+      auto [I1, I2, I3, I3Pow1by3, I3Pow2by3] = computeInvariants(invariants);
+
+      for (auto i : dimensionRange()) {
+        dW1dLambda[i] = 2.0 * (3.0 * pow(lambda_[i], 2.0) - I1) / (3.0 * lambda_[i] * I3Pow1by3);
+        dW2dLambda[i] = -2.0 * (3.0 * (I3 / pow(lambda_[i], 2.0)) - I2) / (3.0 * lambda_[i] * I3Pow2by3);
+      }
+
+      return std::make_pair(dW1dLambda, dW2dLambda);
+    }
+
+    auto secondDerivative() const {
+      const Invariants& invariants            = Impl::invariants(lambda_);
+      auto ddW1dLambda                        = SecondDerivative::Zero().eval();
+      auto ddW2dLambda                        = SecondDerivative::Zero().eval();
+      auto [I1, I2, I3, I3Pow1by3, I3Pow2by3] = computeInvariants(invariants);
+
+      for (auto i : dimensionRange())
+        for (auto j : dimensionRange()) {
+          if (i == j) {
+            ddW1dLambda(i, j) = (2.0 / 9.0) * (5 * I1 - 3 * pow(lambda_[i], 2.0)) / (pow(lambda_[i], 2.0) * I3Pow1by3);
+            ddW2dLambda(i, j) =
+                (2.0 / 9.0) * ((15.0 * I3 / pow(lambda_[i], 2.0)) - I2) / (pow(lambda_[i], 2.0) * I3Pow2by3);
+          } else {
+            ddW1dLambda(i, j) = (4.0 / 9.0) * (I1 - 3 * (pow(lambda_[i], 2.0) + pow(lambda_[j], 2.0))) /
+                                (lambda_[i] * lambda_[j] * I3Pow1by3);
+            ddW2dLambda(i, j) = (-4.0 / 9.0) * (2.0 * I2 - 3 * pow(lambda_[i], 2.0) * pow(lambda_[j], 2.0)) /
+                                (lambda_[i] * lambda_[j] * I3Pow2by3);
+          }
+        }
+      return std::make_pair(ddW1dLambda, ddW2dLambda);
+    }
+
+  private:
+    PrincipalStretches lambda_;
+
+    inline auto dimensionRange() const { return Dune::Hybrid::integralRange(dim); }
+
+    auto computeInvariants(const Invariants& invariants) const {
+      ScalarType I1        = invariants[0];
+      ScalarType I2        = invariants[1];
+      ScalarType I3        = invariants[2];
+      ScalarType I3Pow1by3 = pow(I3, 1.0 / 3.0);
+      ScalarType I3Pow2by3 = pow(I3, 2.0 / 3.0);
+
+      return std::make_tuple(I1, I2, I3, I3Pow1by3, I3Pow2by3);
+    }
+  };
+
+} // namespace Impl
+
 /**
  * \brief Implementation of the InvariantBased material model.
  *
@@ -68,12 +144,13 @@ struct InvariantBasedT
   ScalarType storedEnergyImpl(const PrincipalStretches& lambda) const {
     const Invariants& invariants = Impl::invariants(lambda);
     ScalarType energy{0.0};
-    const auto& pex = exponents(pex_);
-    const auto& qex = exponents(qex_);
+    const auto& pex           = exponents(pex_);
+    const auto& qex           = exponents(qex_);
+    const auto& devInvariants = Impl::DeviatoricInvariants<PrincipalStretches>(lambda);
+    const auto& [W1, W2]      = devInvariants.value();
 
     for (auto i : parameterRange())
-      energy += matParameters_[i] * pow((invariants[0] * pow(invariants[2], -1.0 / 3.0) - 3.0), pex[i]) *
-                pow((invariants[1] * pow(invariants[2], -2.0 / 3.0) - 3.0), qex[i]);
+      energy += matParameters_[i] * pow(W1, pex[i]) * pow(W2, qex[i]);
 
     return energy;
   }
@@ -88,28 +165,17 @@ struct InvariantBasedT
     const Invariants& invariants = Impl::invariants(lambda);
     auto dWdLambda               = FirstDerivative::Zero().eval();
 
-    const auto& mu  = matParameters_;
-    const auto& pex = exponents(pex_);
-    const auto& qex = exponents(qex_);
-
-    ScalarType I1        = invariants[0];
-    ScalarType I2        = invariants[1];
-    ScalarType I3        = invariants[2];
-    ScalarType I3Pow1by3 = pow(I3, 1.0 / 3.0);
-    ScalarType I3Pow2by3 = pow(I3, 2.0 / 3.0);
-    ScalarType I3Pow4by3 = pow(I3, 4.0 / 3.0);
-    ScalarType I4        = -I1 + 3.0 * I3Pow1by3;
-    ScalarType I5        = -I2 + 3.0 * I3Pow2by3;
+    const auto& mu                       = matParameters_;
+    const auto& pex                      = exponents(pex_);
+    const auto& qex                      = exponents(qex_);
+    const auto& devInvariants            = Impl::DeviatoricInvariants<PrincipalStretches>(lambda);
+    const auto& [W1, W2]                 = devInvariants.value();
+    const auto& [dW1dLambda, dW2dLambda] = devInvariants.firstDerivative();
 
     for (auto j : parameterRange())
-      for (auto k : dimensionRange()) {
-        auto factor1 = -2.0 * mu[j] * pow(-I4 / I3Pow1by3, pex[j]) * pow(-I5 / I3Pow2by3, qex[j]);
-        auto factor2 = 1.0 / (pow(lambda[k], 3.0) * I4 * I5);
-        auto factor3 = (-1.0 / 3.0) * pow(lambda[k], 2.0) * I5 * (I1 - 3.0 * pow(lambda[k], 2.0));
-        auto factor4 = I1 * I3 - 3.0 * I3Pow4by3 + I2 * I3Pow1by3 * pow(lambda[k], 2.0) -
-                       (1.0 / 3.0) * I1 * I2 * pow(lambda[k], 2.0);
-        dWdLambda[k] += factor1 * factor2 * (factor3 * pex[j] + factor4 * qex[j]);
-      }
+      for (auto k : dimensionRange())
+        dWdLambda[k] +=
+            mu[j] * pow(W1, pex[j]) * pow(W2, qex[j]) * ((pex[j] * dW1dLambda[k]) / W1 + (qex[j] * dW2dLambda[k]) / W2);
 
     return dWdLambda;
   }
@@ -124,6 +190,28 @@ struct InvariantBasedT
     const Invariants& invariants = Impl::invariants(lambda);
     auto dS                      = SecondDerivative::Zero().eval();
 
+    const auto& mu                         = matParameters_;
+    const auto& pex                        = exponents(pex_);
+    const auto& qex                        = exponents(qex_);
+    const auto& devInvariants              = Impl::DeviatoricInvariants<PrincipalStretches>(lambda);
+    const auto& [W1, W2]                   = devInvariants.value();
+    const auto& [dW1dLambda, dW2dLambda]   = devInvariants.firstDerivative();
+    const auto& [ddW1dLambda, ddW2dLambda] = devInvariants.secondDerivative();
+
+    for (auto p : parameterRange())
+      for (auto i : dimensionRange())
+        for (auto j : dimensionRange()) {
+          auto factor1 = (pex[p] / W1) * (((pex[p] - 1.0) / W1) * dW1dLambda[i] * dW1dLambda[j] + ddW1dLambda(i, j));
+          auto factor2 = (qex[p] / W2) * (((qex[p] - 1.0) / W2) * dW2dLambda[i] * dW2dLambda[j] + ddW2dLambda(i, j));
+          auto factor3 =
+              ((pex[p] * qex[p]) / (W1 * W2)) * (dW1dLambda[i] * dW2dLambda[j] + dW1dLambda[j] * dW2dLambda[i]);
+          auto factor4 = mu[p] * pow(W1, pex[p]) * pow(W2, qex[p]);
+          dS(i, j) += (1.0 / lambda[i]) * (factor4 * (factor1 + factor2 + factor3));
+          if (i == j) {
+            auto factor5 = (pex[p] / W1 * dW1dLambda[i]) + (qex[p] / W2 * dW2dLambda[i]);
+            dS(i, j) -= (1.0 / pow(lambda[i], 2.0)) * factor4 * factor5;
+          }
+        }
     return dS;
   }
 
