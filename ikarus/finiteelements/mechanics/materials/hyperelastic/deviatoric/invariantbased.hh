8fb44aab23d2ac8a0c9e61ee1794ef11bf4ebe4e
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2025-01-31 08:27:25 +0100

    Add More Hyperelastic Materials
    fix dyade
    
    working blatz ko
    
    rework structure part I
    
    interface for volumetric + vf functions
    
    ogden ?
    
    some work on ogden
    
    work on materials
    
    + ad test
    some work that didn't get us anywhere
    
    some more ad
    
    hä
    
    work on interface
    
    WIP on feature/blatzko
    
    documentation + tests
    
    work
    
    fix format + merge ogden
    
    some more documentation
    
    ad works (kind of?)
    
    fix other tests due to new namespace introduction
    Examples + tests work now?
    
    Python Bindings compile?
    
    fix tangentModuli for Ogden model with deviatoric principal stretches
    
    fix spellings
    
    try ogden with multiple parameters, fix bib and remove comments
    
    fix ogden again
    
    fix pythonn
    
    fix testhyperelasticity
    
    works in release mode now, but don't know why
    
    renaming and moving files
    
    start invariant based formulation
    
    add checkDuplicates()
    
    add stress computation for InvariantBased and add autodiff tests
    
    fix
    
    add tangentModuli for general InvariantBased model
    
    add more volumetric functions
    
    refactor and add Arruda-Boyce model
    
    add Gent Model
    
    fix second derivative w.r.t principal stretches for deviatoric functions
    
    refactor BlatzKo
    
    documentation
    
    documentation for Arruda-Boyce model
    
    add AutoDiffMat and refactor tests accordingly
    refactor recoverNeoHookeTest
    
    fix recoverNeoHookeTest
    
    start with an interface to test the results of the deviatoric part
    
    add certain results
    
    fix certain results
    
    rename variables
    
    refactor recoverNeoHookeTest again
    
    fix invariantbased for the undeformed configuration
    
    refactor detC calculation
    
    fix constructors and remove ShearModulus and BulkModulus
    
    add stretchTags
    
    fix safeMultiply
    
    add checkJm in Gent
    
    documentation arruda-boyce and gent
    
    documentation for volumetric functions
    
    update CHANGELOG.md
    fix
    
    more documentation
    
    fix documentation
    
    fix
    
    add docu + some refactoring + add mats to testmaterials.cpp test
    
    fix spelling
    
    add latex shortcuts to \alpha and \lambda
    
    check in invariantbased + precompilation of deviatoric materials
    
    improve exponent check documentation
    
    better template constraints
    
    hotfix
    
    format
    
    autodiff -> experimental + some minor docu things
    typos
    
    fix format + typos
    
    update CHANGELOG.md and about.md
    
    remove trailing space
    
    Get rid of more material models, keep ogden
    
    I hope we can later cherry pick
    
    Code review part I
    
    Ogden: static constexpr
    
    delete tests
    
    Omit later
    
    delete cmake
    
    omit later
    
    license year for non-main branched files
    update comments for clarity; remove unused doubleContraction function
    
    remove "Part" concepts
    
    fix typos
    
    refactor for autodiff -> not working
    
    fix?
    
    fix?
    
    fix after rebase
    
    AD from energy works
    
    fix?
    
    doesn't work
    
    refactor
    
    only run time error exists
    
    fix documentation bib
    
    more fixes
    
    fix?
    
    fix detF
    
    Update CHANGELOG
    
    VF are no template anymore, less restrictivity on types in dev
    
    fixes
    
    fix comment
    
    Using more eigen componentwise notation
    
    fix
    
    minor changes from PR review
    remove computeDirect usage
    
    minor refactor
    
    refactor
    
    remove outdated comments
    
    minor changes from review
    
    delete checkthrow for NH
    
    add missing type alias
    
    add comment
    
    minor fixes from review
    
    forwarding ref
    
    fix NoVolumetricPart
    
    add back materials
    
    enhance hyperelastic material tests and implementations
    
    add more autodiff tests
    
    fix

---
+++ ikarus/finiteelements/mechanics/materials/hyperelastic/deviatoric/invariantbased.hh
@@ -0,0 +1,256 @@
+// SPDX-FileCopyrightText: 2021-2024 The Ikarus Developers mueller@ibb.uni-stuttgart.de
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+/**
+ * \file invariantbased.hh
+ * \brief Implementation of the InvariantBased material model.
+ * \ingroup materials
+ */
+
+#pragma once
+
+#include <ikarus/finiteelements/mechanics/materials/hyperelastic/deviatoric/deviatoricinvariants.hh>
+#include <ikarus/finiteelements/mechanics/materials/materialhelpers.hh>
+#include <ikarus/finiteelements/mechanics/materials/tags.hh>
+#include <ikarus/utils/tensorutils.hh>
+
+namespace Ikarus::Materials {
+
+/**
+ * \brief Implementation of the InvariantBased material model.
+ * \ingroup materials
+ *
+ * \details This is a general model based on deviatoric invariants. It can be used to derive other specific material
+ * models, for instance Mooney-Rivlin and Yeoh models.
+ * The energy is computed as
+ * \f[ \hat{\Psi}(\la_1, \la_2, \la_3) = \sum_{p+q=1}^n{C_{pq} (W_1 - 3)^p (W_2 - 3)^q}, \quad p,q \geq 0. \f]
+ *
+ * \remark See \cite bergstromMechanicsSolidPolymers2015 for details on this material. For information on the deviatoric
+ * invariants \f$ W_1, W_2 \f$, see \ref DeviatoricInvariants.
+ *
+ * \tparam ST_ The underlying scalar type.
+ * \tparam n Number of material parameters
+ */
+template <typename ST_, int n>
+struct InvariantBasedT
+{
+  using ScalarType = ST_;
+
+  template <typename ST = ScalarType>
+  using PrincipalStretches = Eigen::Vector<ST, 3>;
+  template <typename ST = ScalarType>
+  using Invariants = PrincipalStretches<ST>;
+
+  static constexpr int dim              = 3;
+  static constexpr int numMatParameters = n;
+
+  using Exponents          = std::array<std::size_t, numMatParameters>;
+  using MaterialParameters = std::array<double, numMatParameters>;
+
+  template <typename ST = ScalarType>
+  using FirstDerivative = Eigen::Vector<ST, dim>;
+  template <typename ST = ScalarType>
+  using SecondDerivative = Eigen::Matrix<ST, dim, dim>;
+
+  static constexpr auto stretchTag = PrincipalStretchTags::deviatoric;
+
+  [[nodiscard]] constexpr static std::string name() noexcept {
+    return "InvariantBased (n = " + std::to_string(numMatParameters);
+  }
+
+  MaterialParameters materialParametersImpl() const { return matParameters_; }
+
+  const Exponents& pExponents() const { return pex_; }
+
+  const Exponents& qExponents() const { return qex_; }
+
+  /**
+   * \brief Constructor for InvariantBasedT
+   *
+   * \param pex Array of exponents related to the first invariant.
+   * \param qex Array of exponents related to the second invariant.
+   * \param matParameters Array of material parameters.
+   */
+  explicit InvariantBasedT(const Exponents& pex, const Exponents& qex, const MaterialParameters& matParameters)
+      : pex_{pex},
+        qex_{qex},
+        matParameters_{matParameters} {
+    checkExponents();
+  }
+
+  /**
+   * \brief Computes the stored energy in the InvariantBased material model.
+   *
+   * \param lambda principal stretches
+   * \tparam ST the scalartype of the principal stretches
+   * \return ScalarType the energy
+   */
+  template <typename ST = ScalarType>
+  ST storedEnergyImpl(const PrincipalStretches<ST>& lambda) const {
+    const Invariants<ST>& invariants = Impl::invariants(lambda);
+    ST energy{0.0};
+    const auto& pex           = exponents<ST>(pex_);
+    const auto& qex           = exponents<ST>(qex_);
+    const auto& devInvariants = DeviatoricInvariants<PrincipalStretches<ST>>(lambda);
+    auto [W1, W2]             = devInvariants.value();
+    W1 -= 3.0;
+    W2 -= 3.0;
+
+    for (const auto i : parameterRange())
+      energy += matParameters_[i] * pow(W1, pex[i]) * pow(W2, qex[i]);
+
+    return energy;
+  }
+
+  /**
+   * \brief Computes the first derivative of the stored energy function w.r.t. the total principal stretches.
+   *
+   * \param lambda principal stretches
+   * \tparam ST the scalartype of the principal stretches
+   * \return FirstDerivative the first derivative
+   */
+  template <typename ST = ScalarType>
+  FirstDerivative<ST> firstDerivativeImpl(const PrincipalStretches<ST>& lambda) const {
+    const Invariants<ST>& invariants = Impl::invariants(lambda);
+    auto dWdLambda                   = FirstDerivative<ST>::Zero().eval();
+
+    const auto& mu            = matParameters_;
+    const auto& pex           = exponents<ST>(pex_);
+    const auto& qex           = exponents<ST>(qex_);
+    const auto& devInvariants = DeviatoricInvariants<PrincipalStretches<ST>>(lambda);
+    auto [W1, W2]             = devInvariants.value();
+    W1 -= 3.0;
+    W2 -= 3.0;
+    const auto& [dW1dLambda, dW2dLambda] = devInvariants.firstDerivative();
+
+    for (const auto p : parameterRange())
+      for (const auto k : dimensionRange()) {
+        auto W1pm1p = safeMultiply<ST>(pow(W1, pex[p] - 1.0), pex[p]);
+        auto W2qm1q = safeMultiply<ST>(pow(W2, qex[p] - 1.0), qex[p]);
+        dWdLambda[k] +=
+            mu[p] * ((W1pm1p * pow(W2, qex[p]) * dW1dLambda[k]) + (pow(W1, pex[p]) * W2qm1q * dW2dLambda[k]));
+      }
+
+    return dWdLambda;
+  }
+
+  /**
+   * \brief Computes the second derivatives of the stored energy function w.r.t. the total principal stretches.
+   *
+   * \param lambda principal stretches.
+   * \tparam ST the scalartype of the principal stretches
+   * \return SecondDerivative the second derivative
+   */
+  template <typename ST = ScalarType>
+  SecondDerivative<ST> secondDerivativeImpl(const PrincipalStretches<ST>& lambda) const {
+    const Invariants<ST>& invariants = Impl::invariants(lambda);
+    auto dS                          = SecondDerivative<ST>::Zero().eval();
+
+    const auto& mu            = matParameters_;
+    const auto& pex           = exponents<ST>(pex_);
+    const auto& qex           = exponents<ST>(qex_);
+    const auto& devInvariants = DeviatoricInvariants<PrincipalStretches<ST>>(lambda);
+    auto [W1, W2]             = devInvariants.value();
+    W1 -= 3.0;
+    W2 -= 3.0;
+    const auto& [dW1dLambda, dW2dLambda]   = devInvariants.firstDerivative();
+    const auto& [ddW1dLambda, ddW2dLambda] = devInvariants.secondDerivative();
+
+    for (const auto p : parameterRange())
+      for (const auto i : dimensionRange())
+        for (const auto j : dimensionRange()) {
+          auto W1pm1p       = safeMultiply<ST>(pow(W1, pex[p] - 1.0), pex[p]);
+          auto W2qm1q       = safeMultiply<ST>(pow(W2, qex[p] - 1.0), qex[p]);
+          auto W1pm2pp      = safeMultiply<ST>(pow(W1, pex[p] - 2.0), pex[p] * (pex[p] - 1.0));
+          auto W2qm2qq      = safeMultiply<ST>(pow(W2, qex[p] - 2.0), qex[p] * (qex[p] - 1.0));
+          auto dW1W2dlambda = dW1dLambda[i] * dW2dLambda[j] + dW1dLambda[j] * dW2dLambda[i];
+          auto factor1      = (W2qm1q * dW1W2dlambda + pow(W2, qex[p]) * ddW1dLambda(i, j)) * W1pm1p * mu[p];
+          auto factor2      = W2qm1q * ddW2dLambda(i, j) * pow(W1, pex[p]) * mu[p];
+          auto factor3      = W1pm2pp * pow(W2, qex[p]) * dW1dLambda[i] * dW1dLambda[j] * mu[p];
+          auto factor4      = W2qm2qq * pow(W1, pex[p]) * dW2dLambda[i] * dW2dLambda[j] * mu[p];
+          dS(i, j) += factor1 + factor2 + factor3 + factor4;
+          if (i == j) {
+            auto factor5 =
+                mu[p] * (W1pm1p * pow(W2, qex[p]) * dW1dLambda[i] + pow(W1, pex[p]) * W2qm1q * dW2dLambda[i]);
+            dS(i, j) -= (1.0 / lambda[i]) * factor5;
+          }
+        }
+    return dS;
+  }
+
+  /**
+   * \brief Rebinds the material to a different scalar type.
+   * \tparam STO The target scalar type.
+   * \return InvariantBasedT<ScalarTypeOther> The rebound InvariantBased material.
+   */
+  template <typename STO>
+  auto rebind() const {
+    return InvariantBasedT<STO, numMatParameters>(pex_, qex_, matParameters_);
+  }
+
+private:
+  Exponents pex_, qex_;
+  MaterialParameters matParameters_;
+
+  inline static constexpr auto parameterRange() { return Dune::range(numMatParameters); }
+  inline static constexpr auto dimensionRange() { return Dune::range(dim); }
+
+  /** \brief A function to transform the underlying type of the exponents to ScalarType.
+   *
+   * \details If Concepts::AutodiffScalar<ScalarType> is true, then the pow function returns zero instead of one, if
+   * a certain base is raised to an exponent of zero. Hence this transformation is necessary.
+   * \param exp_ An array of exponents of type std::size_t.
+   * \return An array of exponents of type \tparam ScalarType if Concepts::AutodiffScalar<ScalarType> is true, else the
+   * given \param exp_ is returned..
+   */
+  template <typename ST = ScalarType>
+  decltype(auto) exponents(const Exponents& exp_) const {
+    if constexpr (Concepts::AutodiffScalar<ST>) {
+      std::array<ST, numMatParameters> transformedExp_{};
+      std::ranges::transform(exp_, transformedExp_.begin(), [](std::size_t x) { return static_cast<ST>(x); });
+      return transformedExp_;
+    } else
+      return exp_;
+  }
+
+  /**
+   * \brief A function to safely multiply infinity times zero and return zero instead of nan.
+   *
+   * \details In the undeformed configuration, all principal stretches are equal to one. In this scenario, the
+   * deviatoric invariants W1 and W2 are zero. For any positive integer n, 0^{-n} is inf. Multiplying this with 0.0
+   * again leads to nan. In order to circumvent this, in such a scenario, zero is returned instead of nan.
+   *
+   * \remark If inf is multiplied with any other number apart from zero, nan is returned.
+   *
+   * \param x First number to be multiplied.
+   * \param y Second number to be multiplied.
+   * \return ScalarType Either 0.0 or x * y.
+   */
+  template <typename ST = ScalarType>
+  ST safeMultiply(ST x, ST y) const {
+    auto checkInfinityTimeZero = [](double val1, double val2) -> bool {
+      constexpr double tol = 1e-14;
+      return ((std::isinf(val1) && Dune::FloatCmp::eq(val2, 0.0, tol)) ||
+              (std::isinf(val2) && Dune::FloatCmp::eq(val1, 0.0, tol)));
+    };
+
+    // static_cast needed if Concepts::AutodiffScalar<ST>
+    return checkInfinityTimeZero(static_cast<double>(x), static_cast<double>(y)) ? ST{0.0} : x * y;
+  }
+
+  /** \brief check that: \f$ \lnot (q_i = 0 \land p_i = 0) \forall i \f$ */
+  void checkExponents() const {
+    for (const auto i : parameterRange())
+      if (pex_[i] == 0 and qex_[i] == 0)
+        DUNE_THROW(Dune::InvalidStateException, "The exponent q" + std::to_string(i) + "and the exponent p" +
+                                                    std::to_string(i) + "should not be zero at the same time.");
+  }
+};
+
+/**
+ * \brief Alias for InvariantBasedT with double as the default scalar type.
+ */
+template <int n>
+using InvariantBased = InvariantBasedT<double, n>;
+
+} // namespace Ikarus::Materials
