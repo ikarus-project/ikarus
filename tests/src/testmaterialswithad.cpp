37068255fe2f9240728e2f698f8629a3e8cd1852
Henrik Jakob <96132706+henrij22@users.noreply.github.com>
2025-02-20 15:47:58 +0100

    Add More Hyperelastic Materials (#342)
    Co-authored-by: Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>

--- tests/src/testmaterialswithad.cpp
+++ tests/src/testmaterialswithad.cpp
@@ -36,10 +36,18 @@ int main(int argc, char** argv) {
   std::array<double, 3> alpha_og = {1.23, 0.59, 0.18};
   auto ogdenTotal                = makeOgden<3, PrincipalStretchTags::total>(mu_og, alpha_og, Lambda, VF3{});
   auto ogdenDevi                 = makeOgden<3, PrincipalStretchTags::deviatoric>(mu_og, alpha_og, K, VF3{});
+  auto mr                        = makeMooneyRivlin({mu / 2.0, mu / 2.0});
+  auto yeoh                      = makeYeoh({mu / 2.0, mu / 6.0, mu / 3.0});
+  auto ab                        = makeArrudaBoyce({mu, 0.85});
+  auto gent                      = makeGent({mu, 2.5});
 
   t.subTest(checkMaterialByAutoDiff(nh));
   t.subTest(checkMaterialByAutoDiff(ogdenTotal));
   t.subTest(checkMaterialByAutoDiff(ogdenDevi));
+  t.subTest(checkMaterialByAutoDiff(mr));
+  t.subTest(checkMaterialByAutoDiff(yeoh));
+  t.subTest(checkMaterialByAutoDiff(ab));
+  t.subTest(checkMaterialByAutoDiff(gent));
 
   return t.exit();
 }
