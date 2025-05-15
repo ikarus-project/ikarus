a2455cb633ad28b5041c9734d15612c766ae8023
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2025-05-15 08:56:45 +0200

    fix comment

--- ikarus/python/test/runalltests.py
+++ ikarus/python/test/runalltests.py
@@ -21,6 +21,5 @@ runpy.run_module("trusstest", run_name="__main__")
 print("Running vtkwritertest")
 runpy.run_module("vtkwritertest", run_name="__main__")
 
-# Currently disabled because of brakage of dune-iga
 print("Running kltest")
 runpy.run_module("kltest", run_name="__main__")
