7ed14ada9ca1478517d080d78ddc48330a925deb
Tarun Kumar Mitruka Vinod Kumar Mitruka <mitruka@ibb.uni-stuttgart.de>
2025-05-16 09:42:02 +0200

    Create Release v0.4.2 (#380)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from skbuild import setup
 # /dune/dune-common/build-cmake/run-in-dune-env python setup.py sdist
 # /dune/dune-common/build-cmake/run-in-dune-env python -m twine upload dist/* --verbose
 
-ikarusVersion = "0.4.1"
+ikarusVersion = "0.4.2"
 duneVersion = "2.11.0"
 
 metadata = metaData(duneVersion)[1]
